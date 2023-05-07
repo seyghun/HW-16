12c4d9ee539741863187799c61e5be84e2a1f309
seyghun <yaps.gbk@gmail.com>
2023-05-08 00:02:20 +0300

    fixed HW-16

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Post from "./Post";
 
 export default function App() {
   return (
-    <div className='user_info'>
-      <img className='avatar' src={anakin} alt='Anakin'></img>
-      <p id='acc_name'>Anakin Skywalker</p>
-      <img className='check' src={check} alt='check'></img>
-      <p id='profile_tag'>@dart_vader</p>
-      <p id='date'>26 лют.</p>
+    <div>
+      <div className='user_info'>
+        <img className='avatar' src={anakin} alt='Anakin'></img>
+        <p id='acc_name'>Anakin Skywalker</p>
+        <img className='check' src={check} alt='check'></img>
+        <p id='profile_tag'>@dart_vader</p>
+        <p id='date'>26 лют.</p>
+
+      </div>
       <Post />
     </div>
-
   );
 }
 
